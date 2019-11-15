9fd5abcfae02d920200099f0c2d1c6566ad732aa
Alex <55707078+adguez27@users.noreply.github.com>
2019-11-15 02:24:28 +0100

    Maps ready

--- Patinator/app/src/main/java/com/alexd/patinator/MapsActivity2.java
+++ Patinator/app/src/main/java/com/alexd/patinator/MapsActivity2.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -40,8 +41,8 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng UPV = new LatLng(39.4847908, -0.3237898);
+        mMap.addMarker(new MarkerOptions().position(UPV).title("Patinete en la UPV").icon(BitmapDescriptorFactory.fromResource(R.drawable.scooter)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(UPV));
     }
 }
